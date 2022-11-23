314747b08732031c7300a514dcf0f17f13149d5a
Carl Wilund <calle@cwitec.com>
2022-11-23 15:55:16 +0100

    Update tests to jakarta

--- src/it/projects/jaxrs-json-simple/src/main/java/org/codehaus/mojo/servicedocgen/it/jaxrs/json/simple/DemoRestService.java
+++ src/it/projects/jaxrs-json-simple/src/main/java/org/codehaus/mojo/servicedocgen/it/jaxrs/json/simple/DemoRestService.java
@@ -4,15 +4,15 @@ package org.codehaus.mojo.servicedocgen.it.jaxrs.json.simple;
 
 import java.util.List;
 
-import javax.validation.ConstraintViolationException;
-import javax.ws.rs.Consumes;
-import javax.ws.rs.DELETE;
-import javax.ws.rs.GET;
-import javax.ws.rs.POST;
-import javax.ws.rs.Path;
-import javax.ws.rs.PathParam;
-import javax.ws.rs.Produces;
-import javax.ws.rs.core.MediaType;
+import jakarta.validation.ConstraintViolationException;
+import jakarta.ws.rs.Consumes;
+import jakarta.ws.rs.DELETE;
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.POST;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.PathParam;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
 
 /**
  * This is a cool REST-Service to demonstrate <code>servicedocgen-maven-plugin</code>.
